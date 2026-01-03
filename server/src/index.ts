2928305837e418f31f094cc6d86c68f14b51d2a2
Yashwanth N <yashalex03@gmail.com>
2026-01-03 23:16:28 +0530

    project setup - express, mongoose, dev dependencies etc

---
+++ server/src/index.ts
@@ -0,0 +1,29 @@
+import { env } from "./config/processEnv.js";
+import mongoose from "mongoose";
+import express from "express";
+import { authRouter } from "./routes/authRoutes.js";
+
+const app = express();
+const port = env.PORT;
+
+app.use(express.json());
+app.use("/auth", authRouter);
+
+app.get("/", (req, res) => {
+  res.send("hello, frist time from TS");
+});
+
+async function main() {
+  try {
+    await mongoose.connect(env.DB_URL);
+    console.log(`Connected to DB`);
+
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  } catch (err) {
+    console.log(`Error connecting to DB: `, err);
+  }
+}
+
+main();
